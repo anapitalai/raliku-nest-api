17c596a1962fe45fb238a5069757efc7ea89b376
Alois Napitalai <anapitalai@gmail.com>
2019-07-03 14:12:19 +1000

    heroku 3

--- src/geo/geo.controller.ts
+++ src/geo/geo.controller.ts
@@ -3,7 +3,6 @@ import { GeoDTO } from './schema/dto/geo.dto';
 import { GeoService } from './geo.service';
 import { Geo } from 'src/types/geo';
 import { FilesInterceptor } from '@nestjs/platform-express';
-import { RealEstateDTO } from 'dist/realEstate/dto/realEstate.dto';
 
 @Controller('geo')
 export class GeoController {
