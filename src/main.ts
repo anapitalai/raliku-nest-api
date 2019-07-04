d8de7638fef4dbde9acf52dbd19e0c477b483130
Alois Napitalai <anapitalai@gmail.com>
2019-07-04 11:34:47 +1000

    fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as cors from 'cors';
 
 import { AppModule } from './app.module';
 
-if (process.env.NODE_ENV === 'test') {
+/** if (process.env.NODE_ENV === 'test') {
   process.env.MONGO_URI = process.env.MONGO_URI_TEST;
   console.log('----------TESTING IN PROCESS----------');
   console.log('using database', process.env.MONGO_URI);
@@ -16,12 +16,22 @@ const server = Express();
 server.use(cors());
 server.get('/', (req, res) => res.send('ok'));
 server.get('/_ah/health', (req, res) => res.send('ok'));
-server.get('/_ah/start', (req, res) => res.send('ok'));
+server.get('/_ah/start', (req, res) => res.send('ok'));**/
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT);
+ // app.setGlobalPrefix('api');
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
+/*
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
+**/
